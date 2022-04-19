7e048791f60176415b406b40bf63fb66ffd2873b
akky--xxxx <114.s.aki@gmail.com>
2022-04-19 17:59:13 +0900

    chore: adjust setting of sentry

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+const { withSentryConfig } = require("@sentry/nextjs")
+
+const sentryWebpackPluginOptions = {
+  silent: true,
 }
 
-module.exports = nextConfig
+const nextConfig = {
+  reactStrictMode: true,
+  sentry: {
+    disableServerWebpackPlugin: true,
+    disableClientWebpackPlugin: true,
+    hideSourceMaps: true,
+    widenClientFileUpload: true,
+  },
+}
+
+module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions)
