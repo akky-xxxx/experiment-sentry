e31b6c087dbaf6f434c12b8fb1c9ed47d60aa082
akky--xxxx <114.s.aki@gmail.com>
2022-04-22 11:32:19 +0900

    chore: replace hard coding to env value

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -10,7 +10,7 @@ Sentry.init({
   // TODO: 本番ビルド以外でのエラーキャッチを制御する
   dsn: SENTRY_DSN || 'https://cf012c7239a64ae9b1fe44fe2f4a693a@o1210147.ingest.sentry.io/6345791',
   // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1.0,
+  tracesSampleRate: process.env.NEXT_PUBLIC_TRACES_SAMPLE_RATE || 1.0,
   // ...
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
